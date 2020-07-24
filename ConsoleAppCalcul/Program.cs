c64a4058354489d0c5a256572c76a8c712e30c05
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2020-07-24 09:31:42 +0200

    test unitaire init

---
+++ ConsoleAppCalcul/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCalcul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("La somme de 4 et 5 = {0}", (new Calcul()).Addition(4, 5));
+            Console.ReadKey();
+        }
+    }
+}
