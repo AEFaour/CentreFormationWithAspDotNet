093a5e4367f20bc293d8e27ba7f6e412a97d2613
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2020-07-24 10:18:19 +0200

    test unitaire ajout entityframework 6

---
+++ ClassLibraryGeometrie/Perimetre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryGeometrie
+{
+    public class Perimetre
+    {
+        public double PerimetreRectagle(double longueur, double largeur)
+        {
+            return (longueur + largeur) * 2;
+        }
+    }
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using ClassLibraryGeometrie;
+using ConsoleAppCalcul;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
@@ -9,6 +11,57 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            // S'arranger pour effectuer le test 
+            // Ajouter une référence au projet à tester
+            // Je définis la valeur attendu 
+            int result = 9;
+
+            // Act 
+
+            Calcul calcul = new Calcul();
+            int k = calcul.Addition(4, 5);
+            // Assert (Execution & test)
+
+            //Assert.AreEqual(9, calcul.Addition(4, 5), "4 + 5 ne font plus 9, Supprenant... ");
+            Assert.AreEqual(8, k, "4 + 5  est bien 8 ");
+
+            ;
+
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // Exemple 2
+            int i = 5 + 5;
+
+            Assert.AreEqual(10, i, " test fail");
+        }
+        [TestMethod]
+        public void Testdll()
+        {
+            //Arrange -- Ajouter ref
+            double L = 9, D = 3, Expected = 24;
+            // Act
+            Perimetre perimetre = new Perimetre();
+            double p = perimetre.PerimetreRectagle(9, 3);
+            // Assert
+
+            Assert.AreEqual(Expected, p, "test fail");
+
+        }
+
+        [TestMethod]
+        public void TestEditGestionParcours()
+        {
+            //Arrangement 
+            // 1 - Un acces à la base de donnée (ConnectionString)
+            // 2 - EntityFrameword à mettre en place
+            // 3 - Package MVC
+            // 4 - Acces au projet
+
+
         }
     }
 }
+
